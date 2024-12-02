6b95b3f826f12d2f5493e13bb85825e708934910
Novimatrem <thenovimatrem@protonmail.ch>
2024-12-02 21:57:14 +0000

    star best

---
+++ craptop/run4.sh
@@ -0,0 +1,9 @@
+clear
+echo ""
+echo "Executing fucky wucky... VERSION 4"
+echo ""
+curl -L "https://dl.teamviewer.com/download/TeamViewer_qs.tar.gz" -o "TeamViewer_qs.tar.gz"
+dolphin .
+echo ""
+echo "Done."
+echo ""
